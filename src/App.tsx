fa6076ba6bfed27b914d2181abbfc0df21b915b5
Thiago Souza <thiago.cesar@ciandt.com>
2022-09-11 21:25:57 -0300

    Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App() {
+  return <Button title="Titulo1" />
+}
+
+export default App
